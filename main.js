0840262df7c8cd4d07b9898d68e18fb08efcc20f
muratayusuke <info@muratayusuke.com>
2013-06-09 08:45:46 +0000

    add bookmark button

--- main.js
+++ main.js
@@ -318,4 +318,30 @@ $(function(){
 	$(".trigger_filter").click(function(){
 		FBSearch.filterResults();
 	});
+
+	// bookmark event
+	$("#add_bookmark").click(function(){
+		if(navigator.userAgent.indexOf("MSIE") > -1){
+			//Internet Explorer
+            window.external.AddFavorite(toURL,toStr);
+		}else if(navigator.userAgent.indexOf("Lunascape") > -1){
+			//Lunascape
+            alert("[Ctrl]と[G}ボタンを同時に押してください。");
+		}else if(navigator.userAgent.indexOf("Flock") > -1){
+			//Flock
+            window.sidebar.addPanel(toStr,toURL,'');
+		}else if(navigator.userAgent.indexOf("Firefox") > -1){
+			//Firefox
+            window.sidebar.addPanel(toStr,toURL,'');
+		}else if(navigator.userAgent.indexOf("Opera") > -1){
+			//Opera
+            window.open(toURL,'sidebar','title='+toStr);
+		}else if(navigator.userAgent.indexOf("Chrome") > -1){
+			//Chrome,Safari
+            alert("ブラウザ付属のブックマーク機能をご利用ください。[Ctrl]と[D]ボタンを同時に押すと簡単にブックマークできます");
+		}else{
+			//その他
+            alert("ブラウザ付属のブックマーク機能をご利用ください。");
+		}
+	});
 });

46b89c08a5c26091171faaeac2ecac923c65b1a0
muratayusuke <info@muratayusuke.com>
2015-06-17 22:43:24 +0900

    fix login error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,7 @@ require 'dotenv'
 Dotenv::Railtie.load
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  scope = %w(friends_work_history friends_relationships friends_birthday friends_education_history
-             friends_hometown,friends_location,friends_photos).join(',')
+  scope = []
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], client_options: {
     site: 'https://graph.facebook.com/v1.0',
     authorize_url: 'https://www.facebook.com/v1.0/dialog/oauth'
